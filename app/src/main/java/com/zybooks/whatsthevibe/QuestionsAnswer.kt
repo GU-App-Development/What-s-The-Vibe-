e452096c3240d932e84d8422b4473fe78b1c8f2a
adamwhite477 <awhite8@zagmail.gonzaga.edu>
2022-11-16 14:58:58 -0800

    11/16 Answers to Questions now generating. Adam White/Katie Imhof

--- app/src/main/java/com/zybooks/whatsthevibe/QuestionsAnswer.kt
+++ app/src/main/java/com/zybooks/whatsthevibe/QuestionsAnswer.kt
@@ -1,5 +1,130 @@
 package com.zybooks.whatsthevibe
 
-var Questions = arrayListOf<String>("How tired are you?", "Are you hungry?", "When did you go to bed last night?",
-"Do you need motivation right now?", "Do you like to sing along to music?", "What genre are you feeling right now?",
-"Are you feeling sad right now?", "How old are you?", "Are you feeling like listening to holiday music?","What’s your favorite social media?")
+import android.util.ArrayMap
+import java.util.stream.IntStream.range
+
+class QuestionsAnswer{
+
+    /*public var AllAnswers = arrayListOf(arrayListOf<String>())*/
+    public var Questions = arrayListOf<String>()
+
+    val AllAnswers = arrayOf(
+        arrayOf(
+            "I'm sooooo tired",
+            "I could go on a run right now",
+            "50/50",
+            "I'm feelin alright"
+        ),
+        arrayOf(
+            "I just ate I'm all good",
+            "Absolutely starving",
+            "I could eat right now",
+            "Nah, not really"
+        ),
+        arrayOf(
+            "8-10", "11-12", "1-2", "Uhhhhhh"
+        ),
+        arrayOf(
+            "Yeah let's get pumped!!",
+            "Nah, I'm chillin right now",
+            "Yea, I guess",
+            "Absolutely not"),
+        arrayOf(
+            "Yes!",
+            "Not Really",
+            "I'm just trying to chill right now",
+            "I guess so"
+        ),
+        arrayOf(
+            "Give me some throwbacks!", "Something lowkey I can chill too, maybe a little sad",
+            "Rap", "Something up-beat"
+        ),
+        arrayOf(
+            "Yeah...", "Nah I'm good!", "Maybe a little", "Not really"
+        ),
+        arrayOf(
+            "16-21", "22-30", "31-45", "46+"
+        ),
+        arrayOf(
+            "NOPE",
+            "It's about that time!!",
+            "Uhh, sure?",
+            "Not really feeling it"
+        ),
+        arrayOf(
+            "Instagram", "Twitter", "Facebook", "Other"
+        )
+    )
+
+    init {
+
+        Questions.add("How tired are you?")
+        Questions.add("Are you hungry?")
+        Questions.add("When did you go to bed last night?")
+        Questions.add("Do you need motivation right now?")
+        Questions.add("Do you like to sing along to music?")
+        Questions.add("What genre are you feeling right now?")
+        Questions.add("What genre are you feeling right now?")
+        Questions.add("Are you feeling sad right now?")
+        Questions.add("How old are you?")
+        Questions.add("Are you feeling like listening to holiday music?")
+        Questions.add("What’s your favorite social media?")
+
+
+
+        /*val AnswerQuestion1 = arrayOf(
+            "I'm sooooo tired",
+            "I could go on a run right now",
+            "50/50",
+            "I'm feelin alright"
+        )
+        AllAnswers.add(AnswerQuestion1)
+        val AnswerQuestion2 = arrayOf(
+            "I just ate I'm all good",
+            "Absolutely starving",
+            "I could eat right now",
+            "Nah, not really"
+        )
+        AllAnswers.add(AnswerQuestion2)
+        val AnswerQuestion3 = arrayListOf<String>("8-10", "11-12", "1-2", "Uhhhhhh")
+        AllAnswers.add(AnswerQuestion3)
+        val AnswerQuestion4 = arrayListOf<String>(
+            "Yeah let's get pumped!!",
+            "Nah, I'm chillin right now",
+            "Yea, I guess",
+            "Absolutely not"
+        )
+        AllAnswers.add(AnswerQuestion4)
+        val AnswerQuestion5 = arrayListOf<String>(
+            "Yes!",
+            "Not Really",
+            "I'm just trying to chill right now",
+            "I guess so"
+        )
+        AllAnswers.add(AnswerQuestion5)
+        val AnswerQuestion6 = arrayListOf<String>(
+            "Give me some throwbacks!", "Something lowkey I can chill too, maybe a little sad",
+            "Rap", "Something up-beat"
+        )
+        AllAnswers.add(AnswerQuestion6)
+        val AnswerQuestion7 =
+            arrayListOf<String>("Yeah...", "Nah I'm good!", "Maybe a little", "Not really")
+        AllAnswers.add(AnswerQuestion7)
+        val AnswerQuestion8 = arrayListOf<String>("16-21", "22-30", "31-45", "46+")
+        AllAnswers.add(AnswerQuestion8)
+        val AnswerQuestion9 = arrayListOf<String>(
+            "NOPE",
+            "It's about that time!!",
+            "Uhh, sure?",
+            "Not really feeling it"
+        )
+        AllAnswers.add(AnswerQuestion9)
+        val AnswerQuestion10 = arrayListOf<String>("Instagram", "Twitter", "Facebook", "Other")
+        AllAnswers.add(AnswerQuestion10)*/
+    }
+
+
+
+}
+
+
--- app/src/main/java/com/zybooks/whatsthevibe/QuizActivity.kt
+++ app/src/main/java/com/zybooks/whatsthevibe/QuizActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.core.view.children
@@ -15,20 +16,43 @@ class QuizActivity : AppCompatActivity() {
     private lateinit var nextButton : Button
     private lateinit var currentAnswer : String
     private lateinit var QuestionText : TextView
+    private lateinit var Answer1 : RadioButton
+    private lateinit var Answer2 : RadioButton
+    private lateinit var Answer3 : RadioButton
+    private lateinit var Answer4 : RadioButton
     private var selectedAnswers = arrayListOf<String>()
     private var nextButtonClicks : Int = 0
+    private val QuestionsAnswers = QuestionsAnswer()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
-        QuestionText.setText(Questions[0])
+
+
+        //QuestionText.setText(QuestionsAnswer.Questions[0])
         QuestionText = findViewById(R.id.question_text)
+        Answer1 = findViewById(R.id.answer1)
+        Answer2 = findViewById(R.id.answer2)
+        Answer3 = findViewById(R.id.answer3)
+        Answer4 = findViewById(R.id.answer4)
+        if (nextButtonClicks == 0){
+            QuestionText.setText(QuestionsAnswers.Questions[0])
+            Answer1.setText(QuestionsAnswers.AllAnswers[0][0])
+            Answer2.setText(QuestionsAnswers.AllAnswers[0][1])
+            Answer3.setText(QuestionsAnswers.AllAnswers[0][2])
+            Answer4.setText(QuestionsAnswers.AllAnswers[0][3])
+        }
+
 
         nextButton = findViewById(R.id.next_button)
         nextButton.setOnClickListener{view: View ->
             // push answer info in onNextClick!!
             nextButtonClicks++
-            QuestionText.setText(Questions[nextButtonClicks])
+            QuestionText.setText(QuestionsAnswers.Questions[nextButtonClicks])
+            Answer1.setText(QuestionsAnswers.AllAnswers[nextButtonClicks][0])
+            Answer2.setText(QuestionsAnswers.AllAnswers[nextButtonClicks][1])
+            Answer3.setText(QuestionsAnswers.AllAnswers[nextButtonClicks][2])
+            Answer4.setText(QuestionsAnswers.AllAnswers[nextButtonClicks][3])
             onNextClick(view)
         }
 
