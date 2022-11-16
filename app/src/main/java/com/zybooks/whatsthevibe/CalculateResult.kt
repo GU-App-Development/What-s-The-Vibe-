07ef4361b2cfa5373ca45d956dc3449768c35bd8
adamwhite477 <awhite8@zagmail.gonzaga.edu>
2022-11-15 20:13:15 -0800

    11/15 Removing Spotify API Implementation, Program now Compiles. Adam White

--- app/src/main/java/com/zybooks/whatsthevibe/CalculateResult.kt
+++ app/src/main/java/com/zybooks/whatsthevibe/CalculateResult.kt
@@ -1,4 +1,37 @@
 package com.zybooks.whatsthevibe
 
+import android.content.Intent
+
+
 class CalculateResult {
+
+    /*private fun authenticateSpotify() {
+        val builder: AuthorizationRequest.Builder =
+            Builder(CLIENT_ID, AuthorizationResponse.Type.TOKEN, REDIRECT_URI)
+        builder.setScopes(arrayOf<String>(SCOPES))
+        val request: AuthorizationRequest = builder.build()
+        AuthorizationClient.openLoginActivity(this, REQUEST_CODE, request)
+    }*/
+
+    /*protected fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intent)
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            val response: AuthorizationResponse =
+                AuthorizationClient.getResponse(resultCode, intent)
+            when (response.getType()) {
+                TOKEN -> {
+                    editor = getSharedPreferences("SPOTIFY", 0).edit()
+                    editor.putString("token", response.getAccessToken())
+                    Log.d("STARTING", "GOT AUTH TOKEN")
+                    editor.apply()
+                    waitForUserInfo()
+                }
+                ERROR -> {}
+                else -> {}
+            }
+        }
+    }*/
+
 }
