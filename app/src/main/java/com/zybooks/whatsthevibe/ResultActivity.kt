3544aa1a69aaeb918d1931b3cb88de611bd1e4cf
adamwhite477 <awhite8@zagmail.gonzaga.edu>
2022-11-16 15:37:33 -0800

    11/16 Added All Hard-coded songs. Adam White/Katie Imhof

--- app/src/main/java/com/zybooks/whatsthevibe/ResultActivity.kt
+++ app/src/main/java/com/zybooks/whatsthevibe/ResultActivity.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class ResultActivity : AppCompatActivity() {
+
+    val SongPlaylistGeneratorObject = SongPlaylistGenerator()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
---
+++ app/src/main/java/com/zybooks/whatsthevibe/SongPlaylistGenerator.kt
@@ -0,0 +1,17 @@
+package com.zybooks.whatsthevibe
+
+class SongPlaylistGenerator {
+
+    val AllSongs = arrayOf(
+        "DEVESTATED - Joey BADA$$",
+        "Jumpin' Jack Flash - The Rolling Stones",
+        "Hotline Bling - Drake",
+        "Is This It - The Strokes",
+        "Up & Out - Matthew Dear",
+        "Shepherd of Fire - Avenged Sevenfold",
+        "Just the Way You Are - Bruno Mars",
+        "Let it Be - The Beatles"
+    )
+
+
+}
