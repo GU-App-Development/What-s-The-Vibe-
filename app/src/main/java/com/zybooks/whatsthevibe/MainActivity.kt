446bced2f48d7133d7aa5b288dc8e06b97d6f6bc
adamwhite477 <awhite8@zagmail.gonzaga.edu>
2022-11-15 19:00:58 -0800

    11/15 Base Quiz Activity Completed Adam White. Base Activity was created, there is some functionality but more needs to be added.

--- app/src/main/java/com/zybooks/whatsthevibe/MainActivity.kt
+++ app/src/main/java/com/zybooks/whatsthevibe/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.zybooks.whatsthevibe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var beginButton : Button
+    private lateinit var shareButton : Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        beginButton = findViewById(R.id.begin_button)
+        beginButton.setOnClickListener{view: View ->
+            val intent = Intent(this, QuizActivity::class.java)
+            startActivity(intent)
+        }
+
+        shareButton = findViewById(R.id.share_button)
     }
 }
--- app/src/main/java/com/zybooks/whatsthevibe/QuizActivity.kt
+++ app/src/main/java/com/zybooks/whatsthevibe/QuizActivity.kt
@@ -1,11 +1,57 @@
 package com.zybooks.whatsthevibe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.RadioGroup
+import androidx.core.view.children
 
 class QuizActivity : AppCompatActivity() {
+
+    private lateinit var answerRadioGroup : RadioGroup
+    private lateinit var nextButton : Button
+    private lateinit var currentAnswer : String
+    private var selectedAnswers = arrayListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+
+        nextButton = findViewById(R.id.next_button)
+        nextButton.setOnClickListener{view: View ->
+            // push answer info in onNextClick!!
+            onNextClick(view)
+        }
+
+        answerRadioGroup = findViewById(R.id.answer_radio_group)
+
+        for (radioBtn in answerRadioGroup.children) {
+            radioBtn.setOnClickListener(this::onAnswerSelected)
+        }
     }
+
+    private fun onNextClick(view: View){
+        // make sure an answer is selected before we refresh activity!
+        val isAnswerSelected = false
+
+        if (isAnswerSelected && currentAnswer != null){
+            // resets activity
+            selectedAnswers.add(currentAnswer)
+            finish()
+            startActivity(getIntent())
+        }
+    }
+
+    private fun onAnswerSelected(view: View) {
+        currentAnswer = when (view.id) {
+            R.id.answer1 -> "Answer1"
+            R.id.answer2 -> "Answer2"
+            R.id.answer3 -> "Answer3"
+            R.id.answer4 -> "Answer4"
+            else -> "No Answer"
+        }
+    }
+
 }
