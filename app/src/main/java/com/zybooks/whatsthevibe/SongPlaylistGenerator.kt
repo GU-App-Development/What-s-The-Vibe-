845b6a686410007d3a734e6b9a4f3b3fe8489b95
adamwhite477 <awhite8@zagmail.gonzaga.edu>
2022-12-04 14:35:57 -0800

    12/4 Added More Songs! Adam White, Katie Imhof

--- app/src/main/java/com/zybooks/whatsthevibe/SongPlaylistGenerator.kt
+++ app/src/main/java/com/zybooks/whatsthevibe/SongPlaylistGenerator.kt
@@ -32,31 +32,96 @@ class SongPlaylistGenerator {
     )
 
     val ClassicRockSongs = arrayOf(
-        "Classic Rock Song"
+        "House of the Rising Sun by The Animals",
+                "Faithfully by Journey",
+                "Summer Nights by Van Halen",
+                "Stairway to Heaven by Led Zeppelin",
+                "Gold Dust Woman by Fleetwood Mac",
+                "Lonely is the Night by Billy Squier",
+                "Jet Airliner by the Steve Miller Band",
+               "Don’t Talk to Strangers by Rick Springfield",
+                "One of These Nights by the Eagles",
+                "What’s Your Name by Lynyrd Skynyrd",
+                "Keep Your Hands to Yourself by The Georgia Satellites",
+
     )
 
     val HipHopSongs = arrayOf(
-        "Hip Hop Song"
+        "Woodlawn by Amine",
+                "Up by Cardi B",
+                "Jefe by T.I., Meek Mill",
+                "Freeze Tag by Tarrance Martin",
+                "We Go Up by Nicki Minaj, Fivio Foreign",
+                "trademark usa by Baby Keem",
+                "Payday by Doja Cat, Young Thug",
+                "Outside by Megan Thee Stallion",
+                "RELENTLESS by Gunna, Lil Uzi Vert",
+                "Trick by Saweetie",
+                "Knock Knock by SoFaygo",
+                "Kickback by Kblast"
+
     )
 
     val HolidaySongs = arrayOf(
-        "Holiday Song"
+        "till christmas day by Forrest., Forrest Frank",
+                "Winter Wonderland by Remi Wolf",
+                "Santa Claus is Comin’ to Town by Lawrence, Stolen Jars",
+    "Winter Things by Ariana Grande",
+    "Mary Did You Know by Jordan Smith",
+    "Christmas in the Sand by Colbie Caillat",
+    "Have Yourself a Merry Little Christmas by Christina Aguilera",
+    "Parade of the Wooden Soldiers by Harry Connick Jr.",
+    "Christmas Tree Farm by Taylor Swift",
+   "Christmas Without You by Ava Max",
+    "My Christmas Wish by Ali Berke",
+    "The Christmas Song by Isaac Ryan Brown"
     )
 
     val JazzSongs = arrayOf(
-        "Jazz Song"
+        "Tetragon by Joe Henderson",
+                "I’ll Be Seeing You by Billie Holiday",
+            "Alone Together Sonny Stitt",
+            "You Don’t Know What Love Is by Anita O’Day",
+    "Four by Miles Davis Quintet",
+    "Solitude by Sonny Rollins",
+    "Stormy Weather by Etta James",
+    "Windows by Chick Corea",
+    "Four Brothers by Art Pepper",
+    "Autumn Nocturne by Lou Donaldson",
+    "Chloe by Joe Pass",
+    "Lazy Bird by John Coltrane"
+
+
     )
 
     val EDMSongs = arrayOf(
-        "EDM Song"
+        "Waiting for Love by Avicii",
+        "Sweet Dreams by Alan Walker, Imanbek",
+        "Crazy by Lone Tusker",
+        "Madness by Wahlstedt",
+        "Wait by Sam Smyers",
+        "Never Going Home by Kungs",
+        "Pew Pow by Nene",
+        "Roses by REDHEDII",
+        "Give It Up by",
+        "Deep Enough by Sam Welch",
+        "Human by Jack Villa",
+        "Basement by Roast Pang"
     )
 
-    val RBSongs = arrayOf(
-        "RB Song"
-    )
 
     val PopSongs = arrayOf(
-        "Pop Song"
+        "Homesick by Noah Kahan",
+                "sheluvme by Tai Verdes",
+                "Sunflower Seeds by Bryce Vine",
+                "Monte Carlo by Remi Wolf",
+                "Would You Mind by PRETTYMUCH",
+                "BUMMER by Cole Tague",
+                "Voices in the Water by Lux Veritas",
+                "Bad Pics by Efi Gjika",
+                "You’d Never Guess by Elle Critch",
+              "One Life by Dermot Kennedy",
+                "Element by Noah Grant"
     )
 
 }
