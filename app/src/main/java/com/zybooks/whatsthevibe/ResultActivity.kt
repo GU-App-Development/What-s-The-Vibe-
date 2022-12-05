cf12ea6b19f66b9c22fb5326fe5f40397ff8e9e9
Katie <kimhof@zagmail.gonzaga.edu>
2022-12-04 20:07:19 -0800

    Added images to be displayed at the result page

--- app/src/main/java/com/zybooks/whatsthevibe/ResultActivity.kt
+++ app/src/main/java/com/zybooks/whatsthevibe/ResultActivity.kt
@@ -1,11 +1,13 @@
 package com.zybooks.whatsthevibe
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import java.lang.Math
 
@@ -16,12 +18,16 @@ class ResultActivity : AppCompatActivity() {
     private lateinit var ShareButton : Button
     private lateinit var RestartButton : Button
     private lateinit var randomSong : String
+    private lateinit var SongImage : ImageView
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
         SongNameText = findViewById(R.id.song_name_text)
+
+        SongImage = findViewById(R.id.result_image)
 
 
         val allAnswers = intent.getStringArrayListExtra("All Answers")
@@ -30,6 +36,9 @@ class ResultActivity : AppCompatActivity() {
         randomSong = findSong(genre)
         SongNameText.setText(randomSong)
         Log.d("TAG", "Selected Genre was... $genre")
+
+        // Set the image
+        setImage(genre)
 
         ShareButton = findViewById(R.id.share_button)
         ShareButton.setOnClickListener{view: View ->
@@ -74,5 +83,18 @@ class ResultActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+    private fun setImage(genre: String?) {
+        when (genre) {
+            "Pop" -> SongImage.setImageResource(R.drawable.pop_music)
+            "Country" -> SongImage.setImageResource(R.drawable.country_music)
+            "ClassicRock" -> SongImage.setImageResource(R.drawable.rock_music)
+            "Indie" -> SongImage.setImageResource(R.drawable.indie_music)
+            "Jazz" -> SongImage.setImageResource(R.drawable.jazz_music)
+            "EDM" -> SongImage.setImageResource(R.drawable.edm_music)
+            "Holiday" -> SongImage.setImageResource(R.drawable.holiday_music)
+            "HipHop" -> SongImage.setImageResource(R.drawable.hip_hop_music)
+            else -> SongImage.setImageResource(R.drawable.pop_music)
+        }
+    }
 
 }
